4910d946c97f7bb48b00c544c94a4a82674cf434
Krzysztof "Bushee" Nowaczyk <bushee01@gmail.com>
2015-08-18 21:52:27 +0200

    updated require.js dependencies

--- src/windows.js
+++ src/windows.js
@@ -1,12 +1,12 @@
 require.config({
     paths: {
-        'models': 'models/',
-        'views': 'views/',
-        'controllers': 'controllers/',
-        'exceptions': 'exceptions/',
-        'templates': 'templates/',
-        'objects': 'objects/',
-        'utils': 'utils/',
+        'models': 'src/models/',
+        'views': 'src/views/',
+        'controllers': 'src/controllers/',
+        'exceptions': 'src/exceptions/',
+        'templates': 'src/templates/',
+        'objects': 'src/objects/',
+        'utils': 'src/utils/',
 
         'jquery': 'libs/jquery-1.10.2.min',
         'backbone': 'libs/backbone-min',
