5222363a0c5c97f5434fa1eb1f17762ff89c595d
Krzysztof "Bushee" Nowaczyk <bushee01@gmail.com>
2015-08-21 22:20:52 +0200

    added watch & uglify tasks

--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,36 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    var sourceFiles = ['src/main.js', 'src/**/*.js'];
 
     grunt.initConfig({
         concat: {
             dist: {
-                src: ['src/**/*.js'],
+                src: sourceFiles,
                 dest: 'dist/windows.js'
             }
+        },
+        uglify: {
+            options: {
+                sourceMap: true,
+                sourceMapIncludeSources: true
+            },
+            dist: {
+                src: sourceFiles,
+                dest: 'dist/windows.min.js'
+            }
+        },
+        watch: {
+            options: {
+                atBegin: true,
+                debounceDelay: 500
+            },
+            files: sourceFiles,
+            tasks: ['build']
         }
     });
 
-    grunt.registerTask('build', ['concat'])
+    grunt.registerTask('build', ['concat', 'uglify'])
 };
