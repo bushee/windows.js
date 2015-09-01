8a4e6b87f5fa1c39c69975d41554365c71fd6a29
Krzysztof "Bushee" Nowaczyk <bushee01@gmail.com>
2015-09-01 21:14:11 +0200

    using less instead of pure css

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,9 @@
+'use strict';
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
 
     var sourceFiles = ['src/main.js', 'src/**/*.js'];
 
@@ -29,8 +31,24 @@ module.exports = function (grunt) {
             },
             files: sourceFiles,
             tasks: ['build']
+        },
+        less: {
+            options: {
+                compress: true,
+                strictMath: true,
+                strictUnits: true,
+                sourceMap: true,
+                sourceMapBasepath: 'dist'
+            },
+            dist: {
+                options: {
+                    sourceMapFilename: 'dist/windows.css.map'
+                },
+                src: 'less/index.less',
+                dest: 'dist/windows.css'
+            }
         }
     });
 
-    grunt.registerTask('build', ['concat', 'uglify'])
+    grunt.registerTask('build', ['concat', 'uglify', 'less'])
 };
